408320daab936a399c96e11fee521e4237ca4a48
Mohamad Osman Goni <osmangoni0827@gmail.com>
2024-10-01 08:30:26 +0600

    update

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ async function run() {
     const ReviewCollection=await UserDB.collection("UserReview")
     app.get('/product', async(req,res)=>{
        const {searchValue,searchCategory}=req.query;
-       console.log(searchValue)
        const queryByName={
         pd_name:{$regex:searchValue, $options:'i'}
        }
@@ -58,12 +57,10 @@ async function run() {
         pd_category:{$regex:searchValue, $options:'i'}
        }
        if(searchCategory!=='undefined'){
-        console.log("search Category:", searchCategory)
         const result= await ShoeCollection.find(queryByCategory).toArray();
        return res.send(result);
        }
        else if(searchValue){
-        console.log("searchValue",searchValue)
       const result= await ShoeCollection.find(queryByName).toArray();
       console.log(result)
        return res.send(result);
